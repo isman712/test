f3264ca6ab5685f99073bafcf15af2b791b07c21
Isman712 <derekisman712@gmail.com>
2024-02-20 21:40:46 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,29 +3,16 @@
 clear
 cd /home/container
 
-clear                                 
-echo -e ".                \e[35m     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⡄⠀⠀		\e[0m \e[31m																												    \e[0m "
-echo -e ".                \e[35m     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡟⠻⣷⠀		\e[0m \e[31m																												    \e[0m "
-echo -e ".                \e[35m    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⡀⠀⠀⢸⡿⣿⠇⠰⠟⠀⠀	\e[0m \e[31m	   ▄▄█▀▀▀▄█  ██                                                              ▀██▀  ▀██▀                  ▄ 	    \e[0m "  
-echo -e ".                \e[35m     ⠀⠀⠀⠀⣀⡤⣤⠶⠛⠉⠉⠀⠀⠉⠉⠛⠲⣤⣤⣄⠀⠀⠀⠀⠀	\e[0m \e[31m	  ▄█▀     ▀  ▄▄▄  ▄▄ ▄▄▄    ▄▄▄▄   ▄▄ ▄▄ ▄▄   ▄▄ ▄▄ ▄▄     ▄▄▄   ▄▄ ▄▄▄       ██    ██    ▄▄▄    ▄▄▄▄  ▄██▄ 	\e[0m " 
-echo -e ".                \e[35m     ⠀⠀⠀⡼⠃⠈⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠁⠈⢧⠀⠀⠀⠀    \e[0m \e[31m     ██          ██  ██  ██  ▀▀ ▄██   ██ ██ ██   ██ ██ ██  ▄█  ▀█▄  ██  ██       ██▀▀▀▀██  ▄█  ▀█▄ ██▄ ▀   ██  	    \e[0m " 
-echo -e ".                \e[35m     ⠀⠀⡼⢁⡆⠀⢀⡀⠀⠀⠀ ⠀⠀⠀⠀⢀⡄⠀⢰⡈⢧⠀⠀⠀    \e[0m \e[31m     ▀█▄      ▄  ██  ██  ██  ▄█▀ ██   ██ ██ ██   ██ ██ ██  ██   ██  ██  ██       ██    ██  ██   ██ ▄ ▀█▄▄  ██   	\e[0m "
-echo -e ".                \e[35m     ⢀⡞⠁⣸⠁⠀⢠⠬⠓⠀⣀⠀⠀⢀⠀⠛⠥⣄⠀⠀⡇⠈⢳⡀⠀	 \e[0m \e[31m     ▀▀█▄▄▄▄▀  ▄██▄ ▄██▄ ██▄ ▀█▄▄▀█▀ ▄██ ██ ██▄ ▄██ ██ ██▄  ▀█▄▄█▀ ▄██▄ ██▄     ▄██▄  ▄██▄  ▀█▄▄█▀ █▀▄▄█▀  ▀█▄▀ 	\e[0m "
-echo -e ".                \e[35m     ⡞⠀⠀⠹⣆⠀⠀⠆⠀⠀⠓⠊⠙⠚⠁⠀⠀⠀⠀⣰⠇⠀⠀ ⢻⠀	 \e[0m \e[31m																												    \e[0m "
-echo -e ".                \e[35m     ⣇⠀⠀⠀⠈⣹⠶⠦⠤⠤⣤⣤⣤⡤⠤⠤⠴⠶⣏⠁⠀⠀⠀⣸⠀	 \e[0m \e[31m									   with love from supporter ❤												    \e[0m "
-echo -e ".                \e[35m     ⠈⠓⠲⠖⠚⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠓⠲⠶⠚⠁⠀	 \e[0m \e[31m																												    \e[0m "
-echo -e ".		          \e[35m                                         \e[0m \e[31m                  																								    \e[0m "
+echo -e ".                \e[35m /\_/\ \e[0m \e[31m  | \  |  ___  ____   ___   ___    ____   |    |  ____    ____   ____  \e[0m "
+echo -e ".                \e[35m( o.o )\e[0m \e[31m  |  \ | |___   |    |     |___|    |     |----| |    |  |____    |    \e[0m "
+echo -e ".                \e[35m > ^ < \e[0m \e[31m  |   \| |___   |    |___  |   |    |     |    | |____|   ____|   |    \e[0m "
 echo " "
 echo " "
-echo -e "\e[33m - Cinammon Host™'s Discord Support Server en discord: https://discord.gg/3TUf9JBVbt \e[0m"
-echo -e "\e[33m - Momo™'s Discord Support Server: https://discord.gg/3TUf9JBVbt \e[0m"
-echo -e "\e[33m - Cinammon™'s Donations: https://www.patreon.com/cinammon \e[0m"
-echo -e "\e[33m - Cinammon™'s Donations: https://www.patreon.com/cinammon \e[0m"
-echo -e "\e[33m - Momo bot's Invitation: https://discord.gg/ezfkXgekw7   \e[0m"
-#echo " "
-#echo " "
+echo -e "\e[33m - Link de nuestro servidor de soporte en discord: https://discord.gg/5KAjH2XVdt \e[0m"
+echo -e "\e[33m - Para realizar donaciones al proyecto: https://paypal.me/sam170703dev \e[0m"
+echo " "
 echo ""
-echo -e ".\e[31m                                         © Cinammon.es 2024 \e[0m"
+echo -e ".\e[31m                                         © Samuel Vergara Martín. 2024 \e[0m"
 echo " "
 echo " "
 sleep 3
@@ -48,44 +35,47 @@ echo " "
 echo " "
 echo -e "\e[33m Cargando configuracion del startup... \e[0m"
 if [[ ! -z ${NODE_PACKAGES} ]]; then 
-    echo -e "\e[33m Instalando paquetes solicitados... \e[0m"
-    /usr/local/bin/npm install ${NODE_PACKAGES};
+	echo -e "\e[33m Instalando paquetes solicitados... \e[0m"
+	/usr/local/bin/npm install ${NODE_PACKAGES};
 else
-    echo -e "\e[32m ✔️ | No hay solicitudes de instalacion de paquetes. \e[0m"
-fi
+	echo -e "\e[32m ✔️ | No hay solicitudes de instalacion de paquetes. \e[0m"
+fi; 
 if [[ ! -z ${UNNODE_PACKAGES} ]]; then 
-    echo -e "\e[33m Desinstalando paquetes solicitados... \e[0m"
-    /usr/local/bin/npm uninstall ${UNNODE_PACKAGES};
+	echo -e "\e[33m Desinstalando paquetes solicitados... \e[0m"
+	/usr/local/bin/npm uninstall ${UNNODE_PACKAGES};
 else
-    echo -e "\e[32m ✔️ | No hay solicitudes de desinstalacion de paquetes. \e[0m"
-fi 
+	echo -e "\e[32m ✔️ | No hay solicitudes de desinstalacion de paquetes. \e[0m"
+fi; 
 echo " "
 echo " "
 echo -e "\e[33m Buscando el archivo de arranque en el /home/container... \e[0m"
 sleep 2
 
-if [[ -f  "${MAIN_FILE}" ]]; then
-    echo -e "\e[32m ✔️ | Detectado: /home/container/${MAIN_FILE} \e[0m"
+if [[ -f  "/home/container/${MAIN_FILE}" ]]; then
+	echo -e "\e[32m ✔️ | Detectado: /home/container/${MAIN_FILE} \e[0m"
     sleep 2
     echo -e "\e[33m Cargando /home/container/${MAIN_FILE}... \e[0m"
     sleep 1
-    /usr/local/bin/node "${MAIN_FILE}"
+	/usr/local/bin/node /home/container/${MAIN_FILE};
+    sleep 100
     exit 0
-fi
+fi;
 if [ -f /home/container/index.js ]; then
-    echo -e "\e[32m ✔️ | Detectado: /home/container/index.js \e[0m"
+	echo -e "\e[32m ✔️ | Detectado: /home/container/index.js \e[0m"
     sleep 2
     echo -e "\e[33m Cargando /home/container/index.js... \e[0m"
     sleep 1
     /usr/local/bin/node /home/container/index.js
+    sleep 100
     exit 0
 fi
 if [ -f /home/container/src/index.js ]; then
-    echo -e "\e[32m ✔️ | Detectado: /home/container/src/index.js \e[0m"
+	echo -e "\e[32m ✔️ | Detectado: /home/container/src/index.js \e[0m"
     sleep 2
     echo -e "\e[33m Cargando /home/container/src/index.js... \e[0m"
     sleep 1
-    /usr/local/bin/node /home/container/src/index.js
+	/usr/local/bin/node /home/container/src/index.js
+    sleep 100
     exit 0
 fi
     
